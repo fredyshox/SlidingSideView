bb433565bbb01691a03615d05a83ceaebe1f9289
Kacper Raczy <gfw.kra@gmail.com>
2017-07-23 00:40:20 +0200

    Initial Commit

---
+++ SlidingDetailView/SlidingDetailView.h
@@ -0,0 +1,19 @@
+//
+//  SlidingDetailView.h
+//  SlidingDetailView
+//
+//  Created by Kacper Raczy on 23.07.2017.
+//  Copyright © 2017 Kacper Raczy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlidingDetailView.
+FOUNDATION_EXPORT double SlidingDetailViewVersionNumber;
+
+//! Project version string for SlidingDetailView.
+FOUNDATION_EXPORT const unsigned char SlidingDetailViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlidingDetailView/PublicHeader.h>
+
+
